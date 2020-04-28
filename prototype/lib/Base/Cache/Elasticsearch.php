b8ef9bf2ac9fdf0ca3fdb51e88589a6b45141a2d
Justin Chen <cxd032404@hotmail.com>
2020-04-28 17:36:56 +0800

    company_id

--- prototype/lib/Base/Cache/Elasticsearch.php
+++ prototype/lib/Base/Cache/Elasticsearch.php
@@ -14,7 +14,7 @@ class Base_Cache_Elasticsearch implements Base_Cache_Interface
         $CacheConf = (@include dirname(dirname(dirname(dirname(dirname(__FILE__)))))."/CommonConfig/cacheConfig.php");
         if(is_array($CacheConf['ES_SERVER']))
         {
-            $this->client = ClientBuilder::create()->setHosts(['127.0.0.1:9200'])->build();
+            $this->client = ClientBuilder::create()->setHosts($CacheConf['ES_SERVER'])->build();
         }
     }
     public function get($id)
@@ -36,6 +36,7 @@ class Base_Cache_Elasticsearch implements Base_Cache_Interface
         foreach($userList as $user)
         {
             $userInfo = $esConfig + ['id'=>$user['id'],"body"=>$user];
+            $userInfo['index'].="_".$user['company_id'];
             $index = $this->client->index($userInfo);
             if(isset($index['_shards']['successful']) && $index['_shards']['successful'] == 1)
             {
