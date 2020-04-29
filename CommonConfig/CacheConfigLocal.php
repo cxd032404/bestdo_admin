275ca61cd9ddbd8fd8b04b9f15c03a3b1190454d
Justin Chen <cxd032404@hotmail.com>
2020-04-29 14:51:56 +0800

    merge

--- CommonConfig/CacheConfigLocal.php
+++ CommonConfig/CacheConfigLocal.php
@@ -1,8 +1,7 @@
 <?php
 return array
 (
-    'ES_SERVER' => array("127.0.0.1:9200","192.168.31.155:9200"),
-
+    'ES_SERVER' => array("192.168.31.155:9200"),
 	'REDIS_SERVER' => array('192.168.31.155'),
 	'REDIS_PORT'   => '6379',
     'REDIS_PASSWORD' => '123456'
