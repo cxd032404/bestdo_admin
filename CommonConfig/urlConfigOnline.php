92944c6a817928f097b476cca564d6812eb9fb63
Justin Chen <cxd032404@hotmail.com>
2020-05-27 10:12:40 +0800

    merge

--- CommonConfig/urlConfigOnline.php
+++ CommonConfig/urlConfigOnline.php
@@ -4,7 +4,7 @@ return array
     'apiUrl' => 'http://api.staffhome.cn',
     'companyUrl' => 'http://www.bestdo.com',
     'adminUrl' => 'http://admin.staffhome.cn',
-    'siteUrl' => 'http://test.staffhome.cn',
+    'siteUrl' => 'http://www.staffhome.cn',
 
 )
 ?>
